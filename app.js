71ddd546c60dd93145936bdfe69f09fccaa07836
Welberth <welberth_henrique@hotmail.com>
2022-03-02 09:17:16 -0300

    Path module added

--- app.js
+++ app.js
@@ -27,6 +27,7 @@
 // Requiring Modules: 
 const express = require('express');
 const app = express();
+const path = require('path');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require ('mongoose');
